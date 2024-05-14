0d9d5e7068fc8a19c4a242c8503a611fd66bbd27
José Gregorio Molina <molinajoseg@gmail.com>
2024-05-14 03:32:36 -0400

    c

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,7 +1,25 @@
-export default function Pokemon() {
+import { Pokemon } from '@/src/pokemons'
+
+interface Props {
+  params: {
+    id: string
+  }
+}
+
+const getPokemon = async (id: string): Promise<Pokemon> => {
+  const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+    cache: 'force-cache',
+  }).then((res) => res.json())
+
+  return pokemon
+}
+
+export default async function Pokemon({ params }: Props) {
+  const pokemon = await getPokemon(params.id)
   return (
     <div>
-      <h1>Hello Page</h1>
+      <h1>Hola Pokemon {params.id}</h1>
+      <div>{JSON.stringify(pokemon)}</div>
     </div>
   )
 }
--- src/pokemons/index.ts
+++ src/pokemons/index.ts
@@ -1,3 +1,4 @@
 export type * from './components/PokemonGrid'
 export type * from './interfaces/pokemons-response'
 export type * from './interfaces/simple-pokemon'
+export type * from './interfaces/pokemon'
