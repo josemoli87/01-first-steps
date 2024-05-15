5aa0addeaa46a11a0f9bf7d5d2acbedd9292fe36
José Gregorio Molina <molinajoseg@gmail.com>
2024-05-15 00:38:46 -0400

    c

--- next.config.mjs
+++ next.config.mjs
@@ -6,8 +6,12 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'images.unsplash.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+      },
     ],
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
